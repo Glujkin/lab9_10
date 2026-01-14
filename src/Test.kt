e99feded3a9ee2fd4a450d92e29ce1cadc8f18ce
Alina <only.up.ak@gmail.com>
2026-01-14 14:53:04 +0300

    Add abstract class

--- src/Test.kt
+++ src/Test.kt
@@ -12,6 +12,34 @@ data class  Item(
         return "Id предмета: $id\nИмя: $name\nКоличество: $quantity\n"
     }
 }
+abstract class Human(val name: String){
+//    fun  hello(){
+//        println("My name is $name")
+//    }
+    abstract var age : Int
+    abstract fun hello()
+
+}
+class Person(name: String, override var age: Int): Human(name){
+
+    override fun hello() {
+        println("My name is $name")
+    }
+
+}
+abstract class Figure {
+    abstract fun perimeter(): Float
+    abstract fun area(): Float
+}
+class Rechangle(val width: Float,val height: Float) : Figure(){
+    override fun perimeter(): Float {
+        return 2 * (width + height)
+    }
+
+    override fun area(): Float {
+        return width * height
+    }
+}
 fun main() {
 //    println(age)
 //    age = 45
@@ -24,4 +52,9 @@ fun main() {
     println(betterSword.toString())
     val(id,name,quantity) = betterSword
     println("Id предмета: $id\nИмя: $name\nКоличество: $quantity\n")
+    val denis: Person = Person("Denis",10)
+    val maksim: Human = Person("Maksim",25)
+    denis.hello()
+    maksim.hello()
+//    val pavel: Human = Human("Pavel")
 }
