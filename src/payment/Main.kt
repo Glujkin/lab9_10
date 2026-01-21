631c8f335a2c33688b790d1236575806c05e8955
Glujkin <kartoheckajaren@gmail.com>
2026-01-21 15:09:04 +0300

    Create payment processing system

---
+++ src/payment/Main.kt
@@ -0,0 +1,33 @@
+package payment
+
+fun main(){
+    val processor = PaymentProcessor()
+    val payments = listOf(
+        Payment("4_111_111_111_111_111",1000, CardType.VISA),
+        Payment("5_111_111_111_111_111",2000, CardType.MASTERCARD),
+        Payment("2_222_222_222_222_222",1500, CardType.MIR),
+        Payment("1234567812345678",500, CardType.UNKNNOWN),
+        Payment("123",3000, CardType.VISA)
+    )
+    println("=== Обработка платежей ===")
+    payments.forEach { payment ->
+        println("\n Платеж ${payment.type}: ${payment.card.take(4)}...,${payment.sum} руб")
+        val result =processor.pay(payment)
+        processor.show(result)
+
+    }
+    println("\n === Работа с enum ===")
+    val cardType = CardType.VISA
+    println("Тип краты: $cardType")
+    println("порядковый номер: ${cardType.ordinal}")
+    println("все типы карт: ${CardType.values().joinToString()}")
+
+    val payment1 = Payment("411111111111", 1000, CardType.VISA)
+    val payment2 = payment1.copy(type = CardType.MASTERCARD, sum = 2000)
+
+    println("\n === Сравнение data class ===")
+    println("Платеж 1: $payment1")
+    println("Платеж 2: $payment2")
+    println("Одинаковые ? ${payment1 == payment2}")
+
+}
